ca660490e7fdc58897c61b132b699acbffc71981
Jatin Naik <jnaik@pivotal.io>
2016-10-13 10:48:32 +0100

    Allow TLS mock server to be started w/ custom cert
    Signed-off-by: Henry Stanley <hstanley@pivotal.io>

--- mockbosh/mockbosh.go
+++ mockbosh/mockbosh.go
@@ -1,6 +1,8 @@
 package mockbosh
 
 import (
+	"crypto/tls"
+	"net/http"
 	"net/http/httptest"
 
 	"github.com/pivotal-cf-experimental/cf-webmock/mockhttp"
@@ -12,4 +14,16 @@ func New() *mockhttp.Server {
 
 func NewTLS() *mockhttp.Server {
 	return mockhttp.StartServer("mock-bosh", httptest.NewTLSServer)
+}
+
+func NewTLSWithCert(cert tls.Certificate) *mockhttp.Server {
+	return mockhttp.StartServer("mock-bosh", func(handler http.Handler) *httptest.Server {
+		ts := httptest.NewUnstartedServer(handler)
+
+		ts.TLS = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+		ts.StartTLS()
+		return ts
+	})
 }
