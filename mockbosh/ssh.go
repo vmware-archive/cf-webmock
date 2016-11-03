997a8a7db11fd6d6453c18b3814deffef5a72be8
Henry Stanley <hstanley@pivotal.io>
2016-11-03 12:26:08 +0000

    Add instance group validation for SSH
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- mockbosh/ssh.go
+++ mockbosh/ssh.go
@@ -10,30 +10,44 @@ import (
 
 type sshMock struct {
 	*mockhttp.MockHttp
+	instanceGroup string
+	callback      func(string, string)
 }
 
 func StartSSHSession(deploymentName string) *sshMock {
-	return &sshMock{
+	mock := &sshMock{
 		MockHttp: mockhttp.NewMockedHttpRequest("POST", fmt.Sprintf("/deployments/%s/ssh", deploymentName)),
 	}
+	mock.SetResponseCallback(mock.verifyRequest)
+	return mock
 }
 
-func CleanupSSHSession(deploymentName string) *sshMock {
-	return &sshMock{
-		MockHttp: mockhttp.NewMockedHttpRequest("POST", fmt.Sprintf("/deployments/%s/ssh", deploymentName)),
-	}
-}
+var CleanupSSHSession = StartSSHSession
 
 func (mock *sshMock) RedirectsToTask(taskID int) *mockhttp.MockHttp {
 	return mock.RedirectsTo(taskURL(taskID))
 }
 
-func (mock *sshMock) SetSSHResponseCallback(callback func(string, string)) *sshMock {
-	mock.SetResponseCallback(func(body []byte) {
-		response := map[string]interface{}{}
-		Expect(json.Unmarshal(body, &response)).To(Succeed())
-		params := response["params"].(map[string]interface{})
-		callback(params["user"].(string), params["public_key"].(string))
-	})
+func (mock *sshMock) ForInstanceGroup(instanceGroup string) *sshMock {
+	mock.instanceGroup = instanceGroup
 	return mock
+}
+func (mock *sshMock) SetSSHResponseCallback(callback func(string, string)) *sshMock {
+	mock.callback = callback
+	return mock
+}
+
+func (mock *sshMock) verifyRequest(body []byte) {
+	response := map[string]interface{}{}
+	Expect(json.Unmarshal(body, &response)).To(Succeed())
+	params := response["params"].(map[string]interface{})
+	target := response["target"].(map[string]interface{})
+
+	if mock.instanceGroup != "" {
+		Expect(target["job"]).To(Equal(mock.instanceGroup))
+	}
+
+	if mock.callback != nil {
+		mock.callback(params["user"].(string), params["public_key"].(string))
+	}
 }
