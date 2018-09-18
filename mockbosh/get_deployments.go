c124e99ac420d3d03522aef111e835f1f3ea6ec6
Jake Klein <jklein@pivotal.io>
2018-09-18 17:03:46 +0100

    Add get_deployments director endpoint
    [#160547760]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

---
+++ mockbosh/get_deployments.go
@@ -0,0 +1,28 @@
+package mockbosh
+
+import (
+	"github.com/pivotal-cf-experimental/cf-webmock/mockhttp"
+)
+
+type getDeploymentMock struct {
+	*mockhttp.MockHttp
+}
+
+type deployment struct {
+	Name string `json:"name"`
+}
+
+func GetDeployments() *getDeploymentMock {
+	return &getDeploymentMock{
+		MockHttp: mockhttp.NewMockedHttpRequest("GET", "/deployments"),
+	}
+}
+
+func (d *getDeploymentMock) RespondsWithListOfDeployments(deployments []string) *mockhttp.MockHttp {
+	var deps []deployment
+
+	for _, dep := range deployments {
+		deps = append(deps, deployment{Name: dep})
+	}
+	return d.RespondsWithJson(deps)
+}
